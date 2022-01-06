02bd90ea598763e78ee47941d2cc55abf5b5a63f
Paul Gottinger <pgottinger@outlook.com>
2022-01-06 15:44:42 -0600

    game over added

--- assets/js/game.js
+++ assets/js/game.js
@@ -91,11 +91,25 @@ var fight = function (enemyName) {
 function startGame() {
   playerName = window.prompt("What is your robot's name?");
   for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    //call fight function with enemy-robot
-    console.log(pickedEnemyName);
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      //pick new enemy to fight based on the index of the enemyNames array
+      var pickedEnemyName = enemyNames[i];
+
+      //reset enemyHealth before starting new fight
+      enemyHealth = 50;
+
+      //use debugger to pause script from running and check what's going on at that moment in the code
+      //debugger;
+
+      //pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+      console.log(pickedEnemyName);
+      fight(pickedEnemyName);
+    } else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
   }
 }
 
@@ -116,4 +130,4 @@ function startGame() {
 //     console.log(i);
 //     console.log(enemyNames[i] + " is at " + i + " index");
 
-// run fight function to start game
+// run fight function to start game}
